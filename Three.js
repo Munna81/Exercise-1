dd912c444dd1588ed0c2fee74a43d03acf4d18da
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-08 23:33:17 +0600

    Create Three.js

---
+++ Three.js
@@ -0,0 +1,10 @@
+let employee1 = {
+    
+    name:"mark",
+    userId:"mark002",
+    email:"munna.ahmed042@gmail.com",
+    address:
+         {street:["12 nabab street road"]},
+    }
+
+console.log(employee1.address.street);
