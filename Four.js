64e245d979644b5860c5d7dd5531e3db67b9fd8f
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-08 23:34:35 +0600

    Create Four.js

---
+++ Four.js
@@ -0,0 +1,24 @@
+let employee1 = {
+    
+    name:"mark",
+    userId:"mark002",
+    email:"munna.ahmed042@gmail.com",
+    address:"12 nabab street road",
+    }
+
+let employee2 = {
+    
+    name:"munna",
+    userId:"munna002",
+    email:"munna.ahmed@gmail.com",
+    address:"8/1 gopi kisan len",
+    }
+
+
+const x = function toEqualOrNot(employee1,employee2){
+
+
+ employee1 !== employee2 ? false:true;
+ 
+ };
+ console.log(employee1==employee2);
