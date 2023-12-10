d7033b7d0092693eb872009265c306634ad2b1e3
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-10 21:38:34 +0600

    Update Two.js

--- Two.js
+++ Two.js
@@ -13,3 +13,14 @@ employee1.address = updatedAddress !== undefined ? updatedAddress:employee1.addr
 function toUpdatsEmoly(updatedEmail,UpdatedAddress) { console.log(updatedEmail,UpdatedAddress); } toUpdatsEmoly("mrmunna@gmail.com","8/1 gopi kisan len road");
 
 
+function toUpdate() =>{
+    if(updatedEmail){
+        employee1.email=updatedEmail
+    }
+    if(updatedAddress){
+        employee1.address=updatedAddress
+    }
+}
+
+
+toUpdate({email:"mrmunna196@gmail.com",address:"8/1 goppi kisan len road"})
