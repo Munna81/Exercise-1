f68248777eada226ab499259fa0a7b2a8d7be6e7
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-12 01:36:38 +0600

    Update Three.js

--- Three.js
+++ Three.js
@@ -14,12 +14,8 @@ function toFindStreet(building,street) {
 for (let key in employee1) {
    if (typeof employee1[key] === "object") {
      for (let nestedKey in employee1[key]) {
-       if(employee1[key][nestedKey]==="builing"){
-           return employee1.address.building
-       } else {
-          return employee1.address.street 
-       }
-     }
+       console.log(employee1[key][nestedKey])
+            }
    } 
  }
 }
