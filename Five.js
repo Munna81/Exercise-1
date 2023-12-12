4d694da0bc480284efd108619ac5bdec21a268ae
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-13 01:38:42 +0600

    Update Five.js

--- Five.js
+++ Five.js
@@ -2,7 +2,37 @@ let student1 = {
    user: {
    name:"munna",
    value:3.56
-   }
+   },
+   
+   user2:{
+   name:"kawser",
+   value:5,
+      },
+  }
+  function toFind (obj,path){
+   const value = path.split(".");
+   return value.reduce((currentObject,value) =>{
+      return currentObject ? currentObject[value] : undefined;
+   },obj)
+}
+
+const Number = toFind(student1,"user2")
+console.log(Number);
+
+
+
+
+
+/*let student1 = {
+   user: {
+   name:"munna",
+   value:3.56
+   },
+   
+   user2:{
+   name:"kawser",
+   value:5,
+      },
   }
   function toFind (obj,path){
    const value = path.split(".");
@@ -13,3 +43,5 @@ let student1 = {
 
 const Number = toFind(student1,"user.value")
 console.log(Number);
+
+*/
