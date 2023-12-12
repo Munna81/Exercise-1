7ea4371aca92d25b86dcf6e5d00aa94307443ed0
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-13 00:05:23 +0600

    Update Five.js

--- Five.js
+++ Five.js
@@ -1,8 +1,10 @@
 let student1 = {
-   
+   user: {
    name:"munna",
    value:3.56
-    
+   }
   }
-const toTransform = Object.keys(student1);
-console.log(toTransform);
+const findValue = function toFind (obj,path){
+   const value = path.split(".");
+   
+}
