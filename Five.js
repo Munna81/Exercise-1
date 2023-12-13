06543c0274c6c81264e5e0d9c7e56f79f7eef994
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-14 00:41:49 +0600

    Update Five.js

--- Five.js
+++ Five.js
@@ -1,14 +1,20 @@
-const student1 = {'faysal ahmed': 'A', 'Munna Dhali': 'A+'}
+const data = {'faysal ahmed': 'A', 'Munna Dhali': 'A+'}
 
+   function toFind(name,grade){
+    object.entries(data).map(([name,grade]) => ({
+      name,grade 
+    }))
+   }     
+toFind();
 
-  function toFind (nastedKey){
+  /*function toFind (nastedKey){
      const value = nastedKey.split(".");
      let result = student1;
      value.forEach(i =>{
         return result = result[i];
      })
      return result;
-  }
+  }*/
 
 
 
