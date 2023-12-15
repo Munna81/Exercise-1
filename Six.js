e197e838e871382f75118ca7c31235985016bdbb
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-16 01:13:13 +0600

    Update Six.js

--- Six.js
+++ Six.js
@@ -1,4 +1,3 @@
-const myCalender = new calender();
 function event (id,title,date,time,attendess){
   this.id = id;
   this.title = title;
@@ -10,6 +9,7 @@ this.event = [];
 this.addEvent = function (event){
   this.events.push(event);
 }
+const myCalender = new calender();
 const event1 = new event (1,"meeting","27-11-2023","06:00","Faysal")
 const event2 = new event (2,"Gossiping","27-11-2023","09:00","Sayham")
 myCalender.addEvent(event1);
