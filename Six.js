91c43b8fb2d13deaa29ae430f2b9c9fe1e0fa4d3
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-18 23:57:25 +0600

    Update Six.js

--- Six.js
+++ Six.js
@@ -21,5 +21,9 @@ const event1 = new Event("work01","meeting","2023-11-27","06:00","Faysal")
 const event2 = new Event("work02","Gossiping","2023-11-27","09:00","Sayham")
 myCalendar.addEvent(event1);
 myCalendar.addEvent(event2);
-console.log(eventsOnDate);
 const eventsOnDate = myCalendar.getEventsOnDate("2023-11-27");
+console.log(eventsOnDate);
+function remove () {
+  this.events.remove(event)
+}
+remove()
