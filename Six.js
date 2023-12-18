9afbe68e80fb7287d433674ab5c60742e93b1ad6
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-19 00:17:15 +0600

    Update Six.js

--- Six.js
+++ Six.js
@@ -1,29 +1,7 @@
-function Event (id,title,date,time,attendess){
-  this.id = id;
-  this.title = title;
-  this.date = date;
-  this.time = time;
-  this.attendess = attendess;
-}
-function Calendar ()  {
-this.events = [];
-this.addEvent = function (event){
-  this.events.push(event);
-}
-  this.getEventsOnDate = function (date) {
-    const eventsOnDate = this.events.filter(event => event.date === date);
-    return eventsOnDate;
-  };
-}
+let myCalendar = [];
 
-const myCalendar = new Calendar();
-const event1 = new Event("work01","meeting","2023-11-27","06:00","Faysal")
-const event2 = new Event("work02","Gossiping","2023-11-27","09:00","Sayham")
-myCalendar.addEvent(event1);
-myCalendar.addEvent(event2);
-const eventsOnDate = myCalendar.getEventsOnDate("2023-11-27");
-console.log(eventsOnDate);
-function remove () {
-  this.events.remove(event)
-}
-remove()
+function addEvents (evenId,title,date,time,attendess){
+  let newEvent = {evenId,title,date,time,attendess};
+   myCalendar.push(newEvent);
+} 
+addEvents();
