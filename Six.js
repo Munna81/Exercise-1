be0a20307a8de9276d714f062a058457cafbc931
Munna81 <143443259+Munna81@users.noreply.github.com>
2023-12-19 01:22:08 +0600

    Update Six.js

--- Six.js
+++ Six.js
@@ -6,11 +6,11 @@ function addEvents (evenId,title,date,time,attendess){
 } 
 addEvents();
 function updateEvent (evenId,updatedinfo){
-  let event = eventCalendar.filter(item => item.evenId === evenId);
+  let event = eventCalendar.find(item => item.evenId === evenId);
   if(event){
     Object.assign(event,updatedinfo);
   }
 }
 function deleteEvent(evenId){
- let reupdatedEvent = eventcalendar.filter(item => item.evenId !== evenId);
+ let reupdatedEvent = eventcalendar.find(item => item.evenId !== evenId);
 } 
